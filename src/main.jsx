5f00861528a2423257f70c7aefc3d1d1013f6313
ryzenmagsino <magsino301107@gmail.com>
2025-05-23 20:57:55 +0800

    neww

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App'; // This now handles all routing
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/SmartWash">
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
