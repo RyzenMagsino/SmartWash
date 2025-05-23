8be7d9dde1f9304d80f3545898141c2bdc90c0e2
ryzenmagsino <magsino301107@gmail.com>
2025-05-23 20:55:58 +0800

    newq

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  base: '/SmartWash/', // 👈 REQUIRED for GitHub Pages
+  base: '/', // 👈 REQUIRED for GitHub Pages
   plugins: [react()],
   resolve: {
     alias: {
